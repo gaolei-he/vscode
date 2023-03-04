e0848a7c00be9fa89d2519b2b3441820726370b1
whoami096 <ahegaolei@163.com>
2023-03-04 10:31:25 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,73 @@
 #include <iostream>
+#include <cstring>
 #include <algorithm>
+#include <vector>
+#include <set>
+#include <bitset>
 #include <queue>
+#include <map>
+#include <stack>
+#include <string>
+#include <random>
+#include <cassert>
+#include <functional>
+#include <iomanip>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#define inf 0x3f3f3f3f
+#define linf 0x3f3f3f3f3f3f3f3fll
+#define endl '\n'
+#define ll long long
 #define int long long
-int a[1000010];
+#define ull unsigned long long
+#define SZ(x) (int)x.size()
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define dec(i, n, a) for (int i = (n); i >= (a); i--)
 using namespace std;
-using ll = long long;
+using pii = pair<int, int>;
+using ar2 = array<int, 2>;
+mt19937 mrand(random_device{}());
+int rnd(int x) { return mrand() % x; }
+const int N = 10 + 1e5, mod = 1e9 + 7;
+int qp(int a, int b, int p = mod)
+{
+    int res = 1;
+    while(b)
+    {
+        if(b & 1) res = res * a % p;
+        b >>= 1;
+        a = a * a % p;
+    }
+    return res;
+}
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+    unordered_map<string, string> mp;
+    rep(i, 1, n)
+    {
+        string a, b;
+        cin >> a >> b;
+        if(mp.count(a)) mp[a] = min(mp[a], b);
+        else mp[a] = b;
+    }
+    rep(i, 1, q)
+    {
+        string a; cin >> a;
+        if(mp.count(a)) cout << mp[a] << endl;
+        else cout << "Not your business, don't ask more!" << endl;
+    }
+}
 signed main()
 {
-    int n; cin >> n;
-    
-    for (int i = 0; i < n; i++) cin >> a[i];
-    int ans = 0;
-    sort(a, a + n);
-    for (int i = 0; i < n; i++) ans += a[i] * (n - i);
-    cout << ans << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin >> t;
+    while(t--)
+        solve();
+
     return 0;
 }
