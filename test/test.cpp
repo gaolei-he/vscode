6cd3af4eb769fb91da62df2d4bfbb161ebc3504d
whoami096 <ahegaolei@163.com>
2023-03-10 12:58:25 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -1,40 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <map>
 using namespace std;
+int idx[2010];
 int main()
 {
     int n; cin >> n;
-    map<int, int> mp;
+    long long ans = 0;
     for (int i = 0; i < n; i++)
     {
         int x; cin >> x;
-        mp[x] ++;
+        for (int j = x + 1; j < 2010; j++)
+        {
+            ans += idx[j];
+        }
+        idx[x] ++;
     }
-    int ma = 0;
-    for(auto& p:mp) ma = max(p.second, ma);
-    for(auto& p:mp)
-        if(p.second == ma)
-            cout << p.first << endl << p.second << endl;
+    cout << ans << endl;
     return 0;
 }
-// int a[2010];
-// int main()
-// {
-//     int n; cin >> n;
-//     vector<int> vec(n);
-//     for (int i = 0; i < n; i++)
-//     {
-//         int x; cin >> x;
-//         vec.push_back(x);
-//         a[x] ++;
-//     }
-//     long long ans = 0;
-//     for(auto x:vec)
-//     {
-//         for(int i=1;i < x;i ++) ans += a[i];
-//         a[x] --;
-//     }
-//     cout << ans << endl;
-//     return 0;
-// }
