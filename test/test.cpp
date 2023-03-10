3c44d51120ac2e33a8b5c19f8944fc2d2248f63b
whoami096 <ahegaolei@163.com>
2023-03-10 17:34:14 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int idx[2010];
+
+string Caesar(string laws,int w)
+{
+
+    string library="abcdefghijklmnopqrstuvwxyz";
+    for(int i=0;i<laws.size();i++)
+    {
+        for(int j=0;j<26;j++)
+        {
+            if(laws[i]==library[j])
+            {
+                int c=(j+w) % 26;
+                // cerr << c << endl;
+                laws[i]=library[c];
+            }
+        }
+    }
+    return laws;
+}
+
 int main()
 {
-    int n; cin >> n;
-    long long ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int x; cin >> x;
-        for (int j = x + 1; j < 2010; j++)
-        {
-            ans += idx[j];
-        }
-        idx[x] ++;
-    }
-    cout << ans << endl;
-    return 0;
+    string laws;
+    int w;
+    cout<<"请输入需要加密的明文"<<endl;
+    cin>>laws;
+   
+    cout<<"请输入秘钥"<<endl;
+    cin>>w;
+    cout<<Caesar(laws,w);
+
 }
