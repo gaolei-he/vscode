e1e6597203cb059006cec98bbe40793bfd785f94
whoami096 <ahegaolei@163.com>
2023-04-15 14:23:11 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -3,22 +3,6 @@ using namespace std;
 
 int main()
 {
-    int n; cin >> n;
-    int b[n] = {0};
-    for (int i = 0; i < n; i++)
-    {
-        cout << b[i] << ' ';
-    }
-    cout << endl << endl;
-    int a[10][10] = {0};
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         cout << a[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // }
-    
+    long long j = (1 << 33);
     return 0;
 }
